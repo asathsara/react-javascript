38fd47f3402dd19a2b74eaa8f710b53b6c8a5a4a
A S <65203960+asathsara@users.noreply.github.com>
2024-08-22 19:25:27 +0530

    Button Events

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,13 @@
 import styles from './Button.module.css'
 
 const Button = ({btnText}) => {
+
+
+    //Events
+    // In the events e means events
+    const handleClick = (e) => e.target.textContent = "Hello";
     return (
-        <button className={`btn btn-primary ${styles.btn}`}>{btnText}</button>
+        <button onClick={(e) =>handleClick(e)} className={`btn btn-primary ${styles.btn}`}>{btnText}</button>
   )
 }
 
